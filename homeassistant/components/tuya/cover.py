0a6dbb3d633afd1ae921380eeac3a98dcf19ff5e
Caliel Lima da Costa <caliel@limadacosta.com>
2020-07-21 18:06:52 +0200

    add new set position behavior

--- homeassistant/components/tuya/cover.py
+++ homeassistant/components/tuya/cover.py
@@ -1,20 +1,39 @@
 """Support for Tuya covers."""
 from homeassistant.components.cover import (
+    ATTR_POSITION,
     DOMAIN as SENSOR_DOMAIN,
     ENTITY_ID_FORMAT,
     SUPPORT_CLOSE,
     SUPPORT_OPEN,
     SUPPORT_STOP,
+    SUPPORT_SET_POSITION,
     CoverEntity,
 )
-from homeassistant.const import CONF_PLATFORM
+
+from homeassistant.const import (
+    CONF_PLATFORM,
+    STATE_CLOSED,
+    STATE_CLOSING,
+    STATE_OPEN,
+    STATE_OPENING,
+    STATE_UNKNOWN,
+)
+
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
+import homeassistant.util.dt as dt
+
+import logging
+import time
 
 from . import TuyaDevice
 from .const import DOMAIN, TUYA_DATA, TUYA_DISCOVERY_NEW
 
 PARALLEL_UPDATES = 0
 
+STATE_ATTR_FULL_MOVIMENT_DURATON = "full_moviment_duration"
+STATE_ATTR_CURRENT_POSITION = "current_position"
+
+logger = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up tuya sensors dynamically through tuya discovery."""
@@ -58,32 +77,211 @@ class TuyaCover(TuyaDevice, CoverEntity):
         super().__init__(tuya, platform)
         self.entity_id = ENTITY_ID_FORMAT.format(tuya.object_id())
 
+        self._full_moviment_duration_in_seconds = 32.5
+        
+        self._status = STATE_UNKNOWN
+        self._current_position = None
+
+        self._current_moviment_start = None
+        self._current_moviment_position = None
+
+    def __set_current_position(self, value):
+        if value is None:
+            self._status = STATE_UNKNOWN
+            self._current_position = None
+        elif value <= 0:
+            self._status = STATE_CLOSED
+            self._current_position = 0
+        elif value >= 100: 
+            self._status = STATE_OPEN
+            self._current_position = 100
+        else:
+            self._status = STATE_UNKNOWN
+            self._current_position = value
+
+        logger.debug(self.entity_id + "> Status: " + str(self._status))
+        logger.debug(self.entity_id + "> CurrentPosition: " + str(self._current_position))
+
+    def __do_open_cover(self):
+        self._tuya.open_cover()
+
+        self._status = STATE_OPENING
+        self._current_position = None
+
+    def __do_close_cover(self):
+        self._tuya.close_cover()
+
+        self._status = STATE_CLOSING
+        self._current_position = None
+
+    def __do_stop_cover(self):
+        now = dt.now()
+
+        self._tuya.stop_cover()
+
+        if self._current_moviment_start is None or self._current_moviment_position is None:
+            self._current_moviment_start = None
+            self._current_moviment_position = None
+            return
+
+        duration = (now - self._current_moviment_start).total_seconds()
+        logger.debug(self.entity_id + "> duration: " + str(duration))
+
+        moviment_range = duration * 100 / self._full_moviment_duration_in_seconds
+        logger.debug(self.entity_id + "> moviment_range: " + str(moviment_range))
+
+        if self._status == STATE_OPENING:
+            self.__set_current_position(self._current_moviment_position + moviment_range)
+        elif self._status == STATE_CLOSING:
+            self.__set_current_position(self._current_moviment_position - moviment_range)
+
+        self._current_moviment_start = None
+        self._current_moviment_position = None
+
+    def __check_timeout_moviment(self):
+        if self._status == STATE_UNKNOWN or self._current_moviment_start is None:
+            self._current_moviment_start = None
+            self._current_moviment_position = None
+            return
+
+        now = dt.now()
+        duration = (now - self._current_moviment_start).total_seconds()
+        logger.debug(self.entity_id + "> duration: " + str(duration))
+
+        if duration < self._full_moviment_duration_in_seconds:
+            return
+
+        if self._status == STATE_OPENING or self._status == STATE_OPEN: 
+            self.__set_current_position(100)
+        elif self._status == STATE_CLOSING or self._status == STATE_CLOSED:
+            self.__set_current_position(0)
+
+        self._current_moviment_start = None
+        self._current_moviment_position = None
+
+    @property
+    def full_moviment_duration_in_seconds(self):
+        """Get full_moviment_duration_in_seconds."""
+        return self._full_moviment_duration_in_seconds
+
     @property
     def supported_features(self):
         """Flag supported features."""
         supported_features = SUPPORT_OPEN | SUPPORT_CLOSE
         if self._tuya.support_stop():
-            supported_features |= SUPPORT_STOP
+            supported_features |= SUPPORT_STOP | SUPPORT_SET_POSITION
+
         return supported_features
+
+    @property
+    def state_attributes(self):
+        """Return the state attributes of the sun."""
+        return {
+            STATE_ATTR_FULL_MOVIMENT_DURATON: self._full_moviment_duration_in_seconds,
+            STATE_ATTR_CURRENT_POSITION: self._current_position
+        }
+
+    @property
+    def is_opening(self):
+        """Return if the cover is opening or not."""
+
+        self.__check_timeout_moviment()
+        return self._status == STATE_OPENING
+
+    @property
+    def is_closing(self):
+        """Return if the cover is closing or not."""
+
+        self.__check_timeout_moviment()
+        return self._status == STATE_CLOSING
 
     @property
     def is_closed(self):
         """Return if the cover is closed or not."""
-        state = self._tuya.state()
-        if state == 1:
-            return False
-        if state == 2:
-            return True
-        return None
+
+        self.__check_timeout_moviment()
+
+        if self._current_position is None:
+            return None
+        return self._current_position == 0
+
+    @property
+    def current_cover_position(self):
+        """Return the current position of cover shutter."""
+        return self._current_position
 
     def open_cover(self, **kwargs):
         """Open the cover."""
-        self._tuya.open_cover()
+        logger.info(self.entity_id + "> open_cover")
+
+        self.__do_stop_cover()
+
+        self._current_moviment_start = dt.now()
+        self._current_moviment_position = self._current_position
+
+        self.__do_open_cover()
 
     def close_cover(self, **kwargs):
         """Close cover."""
-        self._tuya.close_cover()
+        logger.info(self.entity_id + "> close_cover")
+
+        self.__do_stop_cover()
+        
+        self._current_moviment_start = dt.now()
+        self._current_moviment_position = self._current_position
+
+        self.__do_close_cover()
 
     def stop_cover(self, **kwargs):
         """Stop the cover."""
+        logger.info(self.entity_id + "> stop_cover")
+
+        self.__do_stop_cover()
+
+    def set_cover_position(self, **kwargs):
+        """Move the cover to a specific position."""
+        logger.info(self.entity_id + "> set_cover_position")
+
+        self.__do_stop_cover()
+
+        target_position = int(kwargs.get(ATTR_POSITION))
+        if target_position == self._current_position:
+            return
+
+        if target_position <= 5 or (self._current_position is None and target_position < 60):
+            self.__do_close_cover()
+
+            time.sleep(self._full_moviment_duration_in_seconds)
+            self._tuya.stop_cover()
+
+            self.__set_current_position(0)
+
+        elif target_position >= 95 or (self._current_position is None and target_position >= 60):
+            self.__do_open_cover()
+
+            time.sleep(self._full_moviment_duration_in_seconds)
+            self._tuya.stop_cover()
+
+            self.__set_current_position(100)
+
+        distance = target_position - self._current_position
+        logger.debug(self.entity_id + "> distance: " + str(distance))
+
+        if distance >= -5 and distance <= 5:
+            return
+
+        moviment_duration_in_seconds = abs(distance * self._full_moviment_duration_in_seconds / 100.0)
+        logger.debug(self.entity_id + "> moviment_duration_in_seconds: " + str(moviment_duration_in_seconds))
+
+        if distance > 0:
+            self.__do_open_cover()
+        elif distance < 0:
+            self.__do_close_cover()
+
+        time.sleep(moviment_duration_in_seconds)
         self._tuya.stop_cover()
+
+        self.__set_current_position(target_position)
+
+        self._current_moviment_start = None
+        self._current_moviment_position = None
