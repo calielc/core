7d3cfdce66499fdf485b41578444e242d877aeeb
Caliel Lima da Costa <caliel@limadacosta.com>
2020-10-21 23:01:44 +0200

    add switch cache

--- homeassistant/components/tuya/switch.py
+++ homeassistant/components/tuya/switch.py
@@ -7,11 +7,16 @@ from homeassistant.components.switch import (
 from homeassistant.const import CONF_PLATFORM
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
+import homeassistant.util.dt as dt
+
+import logging
+
 from . import TuyaDevice
 from .const import DOMAIN, TUYA_DATA, TUYA_DISCOVERY_NEW
 
 PARALLEL_UPDATES = 0
 
+logger = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up tuya sensors dynamically through tuya discovery."""
@@ -23,10 +28,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         if not dev_ids:
             return
         entities = await hass.async_add_executor_job(
-            _setup_entities,
-            hass,
-            dev_ids,
-            platform,
+            _setup_entities, hass, dev_ids, platform,
         )
         async_add_entities(entities)
 
@@ -58,15 +60,40 @@ class TuyaSwitch(TuyaDevice, SwitchEntity):
         super().__init__(tuya, platform)
         self.entity_id = ENTITY_ID_FORMAT.format(tuya.object_id())
 
+        self._is_on = None
+        self._last_change_is_on = None
+
     @property
     def is_on(self):
         """Return true if switch is on."""
-        return self._tuya.state()
+        if self._is_on is None:
+            return self._tuya.state()
+
+        now = dt.now()
+        duration = (now - self._last_change_is_on).total_seconds()
+        if duration > 30:
+            newState = self._tuya.state()
+            logger.debug(self.entity_id + "> clear cache, was: " + str(self._is_on) + " became: " + str(newState))
+
+            self._is_on = None
+            self._last_change_is_on = None
+
+            return newState
+
+        return self._is_on
 
     def turn_on(self, **kwargs):
         """Turn the switch on."""
         self._tuya.turn_on()
 
+        self._is_on = True
+        self._last_change_is_on = dt.now()
+        logger.debug(self.entity_id + "> turned on")
+
     def turn_off(self, **kwargs):
         """Turn the device off."""
         self._tuya.turn_off()
+
+        self._is_on = False
+        self._last_change_is_on = dt.now()
+        logger.debug(self.entity_id + "> turned off")
